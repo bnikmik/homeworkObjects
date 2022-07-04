cb8b0196cb5ee631470166aa8a31181e0d155ab0
Nikita Bolshakov <bnikmik@gmail.com>
2022-07-04 20:24:55 +0300

    Домашнее задание: Объекты.

---
+++ src/Author.java
@@ -0,0 +1,17 @@
+public class Author {
+    public final String firstName;
+    public final String secondName;
+
+    public Author(String firstName, String secondName) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+}
